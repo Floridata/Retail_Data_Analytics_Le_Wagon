f497dfa11715c4882422f441ecea4396517bfd44
Sabir <fferrando.florian@gmail.com>
2025-03-01 17:46:40 +0000

    Modification de majuscules

--- models/staging/intermediate/int_sales_data_set.sql
+++ models/staging/intermediate/int_sales_data_set.sql
@@ -1,9 +1,9 @@
 SELECT 
     Store, 
     Dept, 
-    Date AS date_date,
+    Date AS Date_date,
     Weekly_Sales, 
     IsHoliday,
-    CONCAT('Store ', CAST(store AS STRING),' - ', CAST(date AS STRING)) AS store_date,
-    CONCAT(CAST(Store AS STRING), '-', CAST(Dept AS STRING), '-', FORMAT_DATE('%Y%m%d', Date)) AS store_dept_date
+    CONCAT('Store ', CAST(store AS STRING),' - ', CAST(date AS STRING)) AS Store_date,
+    CONCAT(CAST(Store AS STRING), '-', CAST(Dept AS STRING), '-', FORMAT_DATE('%Y%m%d', Date)) AS Store_dept_date
 FROM {{ ref('stg_raw__sales') }}
--- models/staging/intermediate/int_stores_data_set.sql
+++ models/staging/intermediate/int_stores_data_set.sql
@@ -2,5 +2,5 @@ SELECT
 Store,
 Type,
 Size as Size_sqf,
-round(Size *  0.092903,2) as Size_sqm
+ROUND(Size *  0.092903,2) as Size_sqm
 FROM {{ ref('stg_raw__store') }}
