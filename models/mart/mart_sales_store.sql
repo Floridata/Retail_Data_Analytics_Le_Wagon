def30f76c95041908458954c3995e2c7e095b404
Tony Raheb <tony.rg@hotmail.fr>
2025-03-08 16:42:13 +0000

    tables finales mart_sales_store et mart_sales_store_date_dept

--- models/mart/mart_sales_store.sql
+++ models/mart/mart_sales_store.sql
@@ -9,6 +9,7 @@ WITH aggregated_data AS (
 Features_agg AS (
     SELECT
         store,
+        AVG(Fuel_Price) AS avg_fuel_price,
         AVG(Temperature_Celsius) AS avg_temperature_celsius,
         AVG(Temperature_Farenheit) AS avg_temperature_farenheit,
         AVG(CPI) AS avg_cpi,
@@ -26,19 +27,20 @@ SELECT
     d.Type,
     d.Size_sqf,
     d.Size_sqm,
-    a.total_weekly_sales,
-    a.avg_weekly_sales,
-    f.sum_markdown1,
-    f.sum_markdown2,
-    f.sum_markdown3,
-    f.sum_markdown4,
-    f.sum_markdown5,
-    (f.sum_markdown1 + f.sum_markdown2 + f.sum_markdown3 + f.sum_markdown4 + f.sum_markdown5) AS total_markdown,
-    (a.total_weekly_sales - (f.sum_markdown1 + f.sum_markdown2 + f.sum_markdown3 + f.sum_markdown4 + f.sum_markdown5)) AS margin,
-    f.avg_temperature_celsius,
-    f.avg_temperature_farenheit,
-    f.avg_cpi,
-    f.avg_unemployment
+    ROUND(a.total_weekly_sales, 2) AS total_weekly_sales,
+    ROUND(a.avg_weekly_sales, 2) AS avg_weekly_sales,
+    ROUND(f.avg_temperature_celsius, 2) AS avg_temperature_celsius,
+    ROUND(f.avg_temperature_farenheit, 2) AS avg_temperature_farenheit,
+    ROUND(f.avg_fuel_price,2) AS avg_fuel_price,
+    ROUND(f.sum_markdown1, 2) AS sum_markdown1,
+    ROUND(f.sum_markdown2, 2) AS sum_markdown2,
+    ROUND(f.sum_markdown3, 2) AS sum_markdown3,
+    ROUND(f.sum_markdown4, 2) AS sum_markdown4,
+    ROUND(f.sum_markdown5, 2) AS sum_markdown5,
+    ROUND(f.sum_markdown1 + f.sum_markdown2 + f.sum_markdown3 + f.sum_markdown4 + f.sum_markdown5, 2) AS total_markdown,
+    ROUND(a.total_weekly_sales - (f.sum_markdown1 + f.sum_markdown2 + f.sum_markdown3 + f.sum_markdown4 + f.sum_markdown5), 2) AS totalsales_totalmarkdown,
+    ROUND(f.avg_cpi, 2) AS avg_cpi,
+    ROUND(f.avg_unemployment, 2) AS avg_unemployment
 FROM {{ ref('int_stores_data_set') }} AS d
 LEFT JOIN aggregated_data AS a ON d.Store = a.Store
 LEFT JOIN Features_agg AS f ON d.Store = f.store
