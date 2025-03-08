c9e33368f7e80f704b00944c20899810899d07a0
Florian Ferrando <fferrando.florian@gmail.com>
2025-03-08 19:29:00 +0000

    Création des dernières tables et validation

--- models/mart/mart_sales_date.sql
+++ models/mart/mart_sales_date.sql
@@ -1,3 +1,4 @@
-SELECT Date_date, SUM(weekly_sales)
-FROM {{ ref('mart_sales_store_date_dept') }}
+SELECT Date_date, SUM(weekly_sales) AS Weekly_sales
+FROM {{ ref('mart_sales_store_date_dept') }} 
 GROUP BY Date_date
+ORDER BY Date_date
